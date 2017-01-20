336ec72607bed4c343b9ccd698efc9e8f1f16fcb
Kay Anar <github@anars.com>
2017-01-20 10:17:23 -0500

    Update

--- source/com/anars/usps4j/USPSClient.java
+++ source/com/anars/usps4j/USPSClient.java
@@ -2,7 +2,33 @@ package com.anars.usps4j;
 
 public class USPSClient {
 
+    private String _userID;
+
+    /**
+     */
     public USPSClient() {
         super();
+    }
+
+    /**
+     * @param userID
+     */
+    public USPSClient(String userID) {
+        super();
+        _userID = userID;
+    }
+
+    /**
+     * @param userID
+     */
+    public void setUserID(String userID) {
+        _userID = userID;
+    }
+
+    /**
+     * @return
+     */
+    public String getUserID() {
+        return (_userID);
     }
 }
